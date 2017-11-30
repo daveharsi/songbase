6e1b7888cfaeb860d8c36c9f5b1870f98413afdf
Devarshi Patel <ddpatel@udel.edu>
2017-11-30 10:37:40 -0500

    Added delete files

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ def deploy():
     coldplay = Artist(name='Coldplay', about='Coldplay is a British rock band.')
     maroon5 = Artist(name='Maroon 5', about='Maroon 5 is an American pop rock band.')
     song1 = Song(name='Yellow', year=2000, lyrics="Look at the stars", artist=coldplay)
-    song2 = Song(name='Sugar', year=2014, lyrics="I'm hurting, baby, I'm broken down", artist=maroon5)
+    song2 = Song(name='Yellow2', year=2000, lyrics="Look at the stars2", artist=coldplay)
     db.session.add(coldplay)
     db.session.add(maroon5)
     db.session.add(song1)
